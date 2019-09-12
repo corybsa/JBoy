41770d71d6d0291f457d3cc5be99f8b591993e46
iHaruku <corybsa@gmail.com>
2019-09-12 10:05:31 -0400

    Added interface elements to choose a GameBoy rom.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,94 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import jboy.disassembler.Disassembler;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main extends Application {
+    private Stage stage;
+    private ListView<String> listView;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        this.stage = primaryStage;
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("JBoy");
+
+        VBox vbox = new VBox();
+        MenuBar menuBar = createMenuBar();
+        this.listView = new ListView<>();
+
+        vbox.getChildren().add(menuBar);
+        vbox.getChildren().add(this.listView);
+        Scene scene = new Scene(vbox, 300, 275);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private MenuBar createMenuBar() {
+        MenuBar mbMenu = new MenuBar();
+        Menu mActions = new Menu("Actions");
+        MenuItem miDisassemble = new MenuItem("Disassemble...");
+        miDisassemble.setOnAction(x -> {
+            FileChooser chooser = new FileChooser();
+            File file = chooser.showOpenDialog(this.stage);
+
+            this.disassemble(file);
+        });
+
+        mActions.getItems().add(miDisassemble);
+        mbMenu.getMenus().add(mActions);
+
+        return mbMenu;
+    }
+
+    private void disassemble(File file) {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
+            Disassembler disassembler = new Disassembler(bytes);
+            disassembler.disassemble();
+
+            Task<Void> task = new Task<>() {
+                @Override
+                protected Void call() {
+                    ArrayList<String> list = disassembler.getDisassemblyList();
+
+                    for(var item : list) {
+                        listView.getItems().add(item);
+                    }
+
+                    return null;
+                }
+            };
+
+            new Thread(task).start();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
