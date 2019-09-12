5362266b1086a4bfcf5cdf19d5ef15b432648d3a
iHaruku <corybsa@gmail.com>
2019-09-12 14:27:50 -0400

    Removed unneeded imports.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,10 +19,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Main extends Application {
     private Stage stage;
