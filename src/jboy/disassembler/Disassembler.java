2d7f93a55916afc03a82b381343d8363b014b7a4
iHaruku <corybsa@gmail.com>
2019-09-12 10:05:03 -0400

    Changed the output to an ArrayList for improved performance.

--- src/jboy/disassembler/Disassembler.java
+++ src/jboy/disassembler/Disassembler.java
@@ -1,5 +1,7 @@
 package jboy.disassembler;
 
+import java.util.ArrayList;
+
 import static jboy.disassembler.Instructions.GB_16BIT_INSTRUCTIONS;
 import static jboy.disassembler.Instructions.GB_8BIT_INSTRUCTIONS;
 
@@ -10,13 +12,17 @@ public class Disassembler {
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
     private byte[] bytes;
-    private StringBuilder sb = new StringBuilder();
+    private ArrayList<String> instructions;
+
+    public Disassembler(byte[] bytes) {
+        this.bytes = bytes;
+        this.instructions = new ArrayList<>();
+    }
 
     /**
-     * Disassembles the rom and prints the op code and the actual command.
-     * @return the op code and op command in the order they are in the rom.
+     * Disassembles the rom and returns a string representation of the disassembly.
      */
-    public String disassemble() {
+    public void disassemble() {
         int pc;
 
         try {
@@ -33,10 +39,16 @@ public class Disassembler {
                 if((this.bytes[pc] & 0xFF) != 0xCB) {
                     ins8Bit = GB_8BIT_INSTRUCTIONS.get(this.bytes[pc]);
                     pc += ins8Bit.getOpSize();
-                    this.sb.append(this.byteToHex(ins8Bit.getOpCode()))
+                    this.instructions.add(
+                            this.byteToHex(ins8Bit.getOpCode())
+                            .concat("\t\t")
+                            .concat(ins8Bit.getOpName())
+                    );
+
+                    /*this.sb.append(this.byteToHex(ins8Bit.getOpCode()))
                             .append("\t\t")
                             .append(ins8Bit.getOpName())
-                            .append("\n");
+                            .append("\n");*/
                 } else {
                     // To get the proper op code we need to take the current byte at the program counter and shift it left 8 bits.
                     // Next we need to get the following byte, so pc + 1 and use bitwise-and 0xFF to pretend that it's unsigned..
@@ -44,22 +56,21 @@ public class Disassembler {
                     short op = (short)((bytes[pc] << 8) | (bytes[pc + 1] & 0xFF));
                     ins16Bit = GB_16BIT_INSTRUCTIONS.get(op);
                     pc += ins16Bit.getOpSize();
-                    this.sb.append(this.shortToHex(ins16Bit.getOpCode()))
+                    this.instructions.add(
+                            this.shortToHex(ins16Bit.getOpCode())
+                                    .concat("\t")
+                                    .concat(ins16Bit.getOpName())
+                    );
+
+                    /*this.sb.append(this.shortToHex(ins16Bit.getOpCode()))
                             .append("\t")
                             .append(ins16Bit.getOpName())
-                            .append("\n");
+                            .append("\n");*/
                 }
             }
         } catch(Exception ex) {
             ex.printStackTrace();
-            return null;
         }
-
-        return this.sb.toString();
-    }
-
-    public void setBytes(byte[] bytes) {
-        this.bytes = bytes;
     }
 
     /**
@@ -93,4 +104,12 @@ public class Disassembler {
     /*private Instruction<Byte> get8BitInstruction(byte code) {
         return (byte)0;
     }*/
+
+    /**
+     * Returns the instructions in an array list.
+     * @return The instructions
+     */
+    public ArrayList<String> getDisassemblyList() {
+        return this.instructions;
+    }
 }
