aa680d522ce20759dfe3fd41c44b2e921d1c3d48
Cory Sandlin <corybsa@gmail.com>
2019-09-25 09:12:22 -0400

    Finished unit tests for op codes 0x00 through 0x0F. Only 485 left....

--- src/test/jboy/system/CPUInstructions0x00_0x0F.java
+++ src/test/jboy/system/CPUInstructions0x00_0x0F.java
@@ -59,7 +59,7 @@ class CPUInstructions0x00_0x0F {
 
     // op code 0x02
     @Test
-    void ld_bc_a_test() {
+    void ld_bcp_a_test() {
         rom[0x100] = 0x01; // ld bc,0x7F50
         rom[0x101] = 0x50;
         rom[0x102] = 0x7F;
@@ -76,7 +76,7 @@ class CPUInstructions0x00_0x0F {
         assertEquals(0x50, cpu.getA(), "The A register should equal 0x50.");
 
         cpu.tick();
-        assertEquals(0x50, memory.getByteAt(cpu.getBC()), "The BC register should equal 0x50.");
+        assertEquals(0x50, memory.getByteAt(cpu.getBC()), "The memory address pointed to by BC should equal 0x50.");
         assertEquals(0x106, cpu.getPC(), "PC should equal 0x106.");
     }
 
@@ -211,26 +211,57 @@ class CPUInstructions0x00_0x0F {
     // op code 0x07
     @Test
     void rlca_test() {
-        rom[0x100] = 0x3E; // ld a,0xAA
-        rom[0x101] = 0xAA;
+        rom[0x100] = 0x3E; // ld a,0x85
+        rom[0x101] = 0x85;
         rom[0x102] = 0x07; // rcla
 
         memory.loadROM(rom);
 
         cpu.tick();
         cpu.tick();
-        assertEquals(0b01010101, cpu.getA(), "The A register should equal AA shifted left 1 bit.");
-        assertEquals(CPU.FLAG_CARRY, cpu.getF(), "The carry flag should be set.");
+        assertEquals(0x0A, cpu.getA(), "The A register should equal 0x0A.");
+        assertEquals(CPU.FLAG_CARRY, cpu.getF(), "The CARRY flag should be set.");
         assertEquals(0x103, cpu.getPC(), "PC should equal 0x104.");
     }
 
     // op code 0x08
     @Test
-    void ld_xxp_sp_test() {}
+    void ld_xxp_sp_test() {
+        rom[0x100] = 0x31; // ld sp,0xFFF8
+        rom[0x101] = 0xF8;
+        rom[0x102] = 0xFF;
+        rom[0x103] = 0x08; // ld (0xC100),sp
+        rom[0x104] = 0x00;
+        rom[0x105] = 0xC1;
+
+        memory.loadROM(rom);
+
+        cpu.tick();
+        cpu.tick();
+        assertEquals(0xFFF8, memory.getByteAt(0xC100), "The memory address pointed to by xx should equal 0xFFF8");
+        assertEquals(0x106, cpu.getPC(), "PC should equal 0x106");
+    }
 
     // op code 0x09
     @Test
-    void add_hl_bc_test() {}
+    void add_hl_bc_test() {
+        rom[0x100] = 0x01; // ld bc,0x0605
+        rom[0x101] = 0x05;
+        rom[0x102] = 0x06;
+        rom[0x103] = 0x21; // ld hl,0x8A23
+        rom[0x104] = 0x23;
+        rom[0x105] = 0x8A;
+        rom[0x106] = 0x09; // add hl,bc
+
+        memory.loadROM(rom);
+
+        cpu.tick();
+        cpu.tick();
+        cpu.tick();
+        assertEquals(0x9028, cpu.getHL(), "The HL register should equal 0x9028.");
+        assertEquals(CPU.FLAG_HALF, cpu.getF(), "The HALF_CARRY flag should be set.");
+        assertEquals(0x106, cpu.getPC(), "PC should equal 0x106");
+    }
 
     // op code 0x0A
     @Test
@@ -247,7 +278,7 @@ class CPUInstructions0x00_0x0F {
         cpu.tick();
         cpu.tick();
         assertEquals(0x12, cpu.getA(), "The A register should equal 0x12.");
-        assertEquals(0x104, cpu.getPC(), "PC should equal 0x103");
+        assertEquals(0x104, cpu.getPC(), "PC should equal 0x104");
     }
 
     // op code 0x0B
@@ -380,5 +411,27 @@ class CPUInstructions0x00_0x0F {
 
     // op code 0x0F
     @Test
-    void rrca_test() {}
+    void rrca_test() {
+        rom[0x100] = 0x3E; // ld a,0x3B
+        rom[0x101] = 0x3B;
+        rom[0x102] = 0x0F; // rrca
+
+        cpu.tick();
+        cpu.tick();
+        assertEquals(0x9D, cpu.getA(), "The A register should equal 0x9D.");
+        assertEquals(CPU.FLAG_CARRY, cpu.getF(), "The CARRY flag should be set.");
+        assertEquals(0x103, cpu.getPC(), "PC should equal 0x103.");
+
+        cpu.setPC(0x100);
+
+        rom[0x100] = 0x3E; // ld a,0x3B
+        rom[0x101] = 0xAA;
+        rom[0x102] = 0x0F; // rrca
+
+        cpu.tick();
+        cpu.tick();
+        assertEquals(0x55, cpu.getA(), "The A register should equal 0x55.");
+        assertEquals(0x00, cpu.getF(), "No flags should be set.");
+        assertEquals(0x103, cpu.getPC(), "PC should equal 0x103.");
+    }
 }
